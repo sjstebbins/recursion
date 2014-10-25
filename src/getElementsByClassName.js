a255a2d9ee98273b0837748d8ce5b22c5238fdcf
spencer stebbins <sjfstebbins@gmail.com>
2014-10-24 23:47:03 -0500

    completed prework (one clause missing on JSON and need some work on GetElementByClassName)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  // console.log(this.document);
+  var nodeScan = function(node, name){
+    console.log(node);
+    if (node.childElementCount === 0){
+      if (node.classList.contains(name)){
+        results.push(node);
+      }
+    } else {
+      for (var child in node.childNodes) {
+        nodeScan(child,className);
+      }
+    }
+  };
+  var results = [];
+  nodeScan(this.document,className);
+  return results;
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,49 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var out = "";
+  var stringify = function(node){
+    if (node == null){
+      out += "null";
+      return;
+    } else if (node.constructor === Number){
+      out += node.toString();
+    } else if (node.constructor === String){
+      out += '"' + node + '"';
+    } else if (node.constructor === Boolean){
+      console.log(node);
+      if (node === true){
+        out += "true";
+      } else {
+        out += "false";
+      }
+    } else if (node.constructor === Array){
+      out += "[";
+      for (var i = 0; i < node.length;i++){
+        stringify(node[i]);
+        if (i < node.length -1){
+          out += ",";
+        }
+      }
+      out += "]";
+    } else if (node.constructor === Object){
+      var needsComma = false;
+      out += "{";
+      for (var i in node){
+        if (node[i] == undefined || node !== null && node.constructor === Function){}
+        if (needsComma){
+          out += ",";
+        }
+        needsComma = true;
+        out += '"' + i + '":';
+        stringify(node[i]);
+      }
+      out += "}";
+    } else if (node.constructor === Function){
+    } else if (node == undefined){
+    }
+  }
+  stringify(obj);
+  console.log(out);
+  return out;
 };
